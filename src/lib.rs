70c2f66ea00b86bb9bb5bd4f477aafee62f364dc
raymax0x <recognizeyourself909@gmail.com>
2025-06-24 18:40:21 +0530

    understanding slice

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ pub fn say_hello() {
 }
 
 pub fn print() {
-    let numbers = [1,2,3,4,5];
-    output_sequence(numbers);
+    let vector_numbers = vec![1,2,3,4,5];
+    output_sequence(&vector_numbers);
+
+    let array_numbers = [1,2,3,4,5];
+    output_sequence(&array_numbers);
 }
 
-fn output_sequence(numbers: [u8; 5]){
-    for n in numbers.iter() {
+fn output_sequence(numbers: &[u8]){
+    for n in numbers {
         println!("{}", n);
     }
 }
