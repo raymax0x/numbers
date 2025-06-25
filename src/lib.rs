ad600bd397dba9a2534463fe88b2484f90db25d0
raymax0x <recognizeyourself909@gmail.com>
2025-06-25 18:53:47 +0530

    Learned testing rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,9 @@
 pub fn print(limit: u8) {
-    let numbers = generate_sequence1(limit);
+    let numbers = generate_sequence(limit);
     output_sequence(&numbers);
 }
 
 fn generate_sequence(limit: u8) -> Vec<u8> {
-    let mut numbers = Vec::new();
-
-    for n in 1..=limit {
-        numbers.push(n);
-    }
-    numbers
-}
-
-// you can replace the whole generate sequence function from this
-fn generate_sequence1(limit: u8) -> Vec<u8> {
     (1..=limit).collect()
 }
 
@@ -23,9 +13,8 @@ fn output_sequence(numbers: &[u8]) {
     }
 }
 
-// how to slice an array
-// fn get_subset() {
-//     let numbers = [1, 2, 3, 4, 5];
-//     let subset = &numbers[1..3];
-//     output_sequence(&subset);
-// }
+#[test]
+fn generate_sequence_should_work() {
+    let result = generate_sequence(3);
+    assert_eq!(result, &[1, 2, 3]);
+}
